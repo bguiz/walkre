896f1325ec2d4ba750c91c5674fa579754cc4846
Brendan Graetz <brendan.graetz@gmail.com>
2013-06-20 20:33:21 +1000

    =BG= serve up a static index html file

--- server/app.js
+++ server/app.js
@@ -2,13 +2,16 @@
 //20130620 Brendan Graetz
 
 var http = require('http');
+var filesys = require('fs');
 
 var server = http.createServer();
 
 server.on('request', function(req, resp) {
-  resp.writeHead(200);
-  resp.write('Walkre\n');
-  resp.end();
+  resp.writeHead({'Content-Type': 'text/event-stream'});
+  filesys.readFile('../static/index.html', function(err, data) {
+    resp.write(data);
+    resp.end();
+  })
 });
 
 server.listen(9999);
