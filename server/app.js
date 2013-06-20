b6666460646d86c18f8a07e5ff4680f7bd758855
Brendan Graetz <brendan.graetz@gmail.com>
2013-06-20 20:44:25 +1000

    =BG= port number accepted as a command line parameter

--- server/app.js
+++ server/app.js
@@ -4,14 +4,28 @@
 var http = require('http');
 var filesys = require('fs');
 
+var portNumber = 9999;
+
+process.argv.forEach(function(token) {
+  var kv = token.split(':');
+  if (kv.length === 2) {
+    if (kv[0] === 'port') {
+      portNumber = parseInt(kv[1], 10);
+    }
+  }
+});
+
 var server = http.createServer();
 
 server.on('request', function(req, resp) {
-  resp.writeHead({'Content-Type': 'text/event-stream'});
+  resp.writeHead({
+    'Content-Type': 'text/event-stream'
+  });
   filesys.readFile('../static/index.html', function(err, data) {
     resp.write(data);
     resp.end();
   })
 });
 
-server.listen(9999);
+server.listen(portNumber);
+console.log('Listening on port', portNumber);
