4843ca4ca335e67afa998b9456a363c574f3c942
Brendan Graetz <brendan.graetz@gmail.com>
2013-06-20 22:25:51 +1000

    =BG= split static pages functions into a separate module

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@
 //20130620 Brendan Graetz
 
 var http = require('http');
-var filesys = require('fs');
+
+var staticPages = require('./staticPages').staticPages;
 
 var portNumber = 9999;
 
@@ -16,36 +17,6 @@ process.argv.forEach(function(token) {
 });
 
 var server = http.createServer();
-
-var staticPages = {
-  '/': function(req, resp) {
-    resp.writeHeader(200, {
-      'Content-Type': 'text/html'
-    });
-    var fileReader = filesys.createReadStream('../static/index.html');
-    fileReader.pipe(resp, {
-      end: false
-    });
-    fileReader.on('end', function() {
-      console.log(req.url, 'served at:',(Date.now()));
-      resp.end();
-    });
-  },
-  '/favicon.ico': function(req, resp) {
-    resp.writeHeader(200, {
-      'Content-Type': 'image/x-icon'
-    });
-    var fileReader = filesys.createReadStream('../static/favicon.ico');
-    fileReader.pipe(resp);
-  },
-  'notfound': function(req, resp) {
-    resp.writeHeader(404, {
-      'Content-Type': 'text/plain'
-    });
-    resp.write('Could not find: ' + req.url);
-    resp.end();
-  }
-};
 
 server.on('request', function(req, resp) {
   var responder = staticPages[req.url];
