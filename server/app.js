4df05cc772041128f8b30d9b47b122bc4bd54e74
Brendan Graetz <brendan.graetz@gmail.com>
2013-06-22 02:13:49 +1000

    =BG= chained a couple of connect middlewares before the actual POST api response function to read all responses as strings irrespective of content type sent in, and another to parse that content as JSON

--- server/app.js
+++ server/app.js
@@ -18,16 +18,80 @@ process.argv.forEach(function(token) {
   }
 });
 
-var server = express();
+var defaultContentTypeMiddleware = function(defaultContentType) {
+  var contentTypeMiddleware = function(req, resp, next) {
+    req.headers['content-type'] = req.headers['content-type'] || defaultContentType;
+    next();
+  };
+  return contentTypeMiddleware;
+};
 
+var maxContentLength = 1e6; //~1mb
+var readRequestDataAsStringMW = function(req, resp, next) {
+  console.log('readRequestDataAsStringMW');
+  req.content = '';
+  var contentLength = 0;
+  req.on('data', function(data) {
+    req.content += data;
+    contentLength += data.length;
+    if (contentLength > maxContentLength) {
+      resp.contentType('application/json');
+      resp.send(406, JSON.stringify({
+        error: 'Data exceeded size limit'
+      }));
+      req.connection.destroy(); //without calling next()
+    }
+  });
+  req.on('end', function() {
+    next();
+  });
+};
+
+var acceptOnlyJsonMW = function(req, resp, next) {
+  console.log('acceptOnlyJsonMW', req.content);
+  try {
+    req.json = JSON.parse(req.content);
+  }
+  catch (exc) {
+    //handled in finally block
+  }
+  finally {
+    if (req.json) {
+      next();
+    }
+    else
+    {
+      resp.contentType('application/json');
+      resp.send(406, JSON.stringify({
+        error: 'Data was invalid JSON'
+      }));
+      req.connection.destroy(); //without calling next()
+    }
+  }
+};
+
+var server = express();
 server.use(express.static(__dirname + '/../static'));
 
-server.get('/api/v1/:entity', function(req, resp) {
-  var entity = req.params.entity;
+server.get('/api/echo', function(req, resp) {
+  resp.contentType('application/json');
   resp.send(200, {
-    entityName: entity,
     query: (req.query)
   });
+});
+
+/*
+e.g.
+curl -i -H "Content-Type: application/json" \
+  -X POST -d '{"p":[1,1,2,3,5],"a":{"b":{"c":5}}}' \
+  http://localhost:9876/api/v1
+*/
+server.post('/api/v1', [readRequestDataAsStringMW, acceptOnlyJsonMW], function(req, resp) {
+  var out = {
+    'request': req.json
+  };
+  console.log(JSON.stringify(out));
+  resp.send(200, JSON.stringify(out));
 });
 
 server.listen(portNumber);
